949d3ccde638371af86376ad1d50d19d15db4579
c0a220657d <c0a220657d@edu.teu.ac.jp>
2023-11-21 16:34:54 +0900

    ensyuukadai

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,19 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     ton03 = pg.transform.flip(pg.image.load("ex01/fig/3.png"),True,False)
     ton03a = pg.transform.rotozoom(ton03,10,1)
-    ton_li = [ton03,ton03a]
+    ton_li = [pg.transform.rotozoom(ton03,i,1) for i in range(-10,11,2)]
+    ton_li_r = reversed(ton_li)
+    ton_li += ton_li_r
     tmr = 0
     bg_x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         screen.blit(bg_img, [bg_x, 0])
-        screen.blit(bg_img, [1600+bg_x, 0])
-        screen.blit(ton_li[tmr%2],[300,200])
-        bg_x = (tmr%1600)*-1
+        screen.blit(pg.transform.flip(bg_img,True,False), [1600+bg_x, 0])
+        screen.blit(bg_img, [3200+bg_x, 0])
+        screen.blit(ton_li[tmr%len(ton_li)],[300,200])
+        bg_x = (tmr%3200)*-1
         pg.display.update()
         tmr += 1        
         clock.tick(100)
